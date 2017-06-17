79948e5b379f5c0e890dbd01299a77a9c8dbfd7a
iamkevingreen <thedudem4n@gmail.com>
2017-06-17 12:41:16 -0400

    initial release

---
+++ package/README.md
@@ -0,0 +1,22 @@
+# rimp
+Small library for handling mailchimp subscriptions on the client side in react. This is
+not a documented solution within mailchimp however it does still `work`
+
+[![js-standard-style](https://cdn.rawgit.com/feross/standard/master/badge.svg)](http://standardjs.com)
+
+## Usage
+Grid with four items:
+
+```javascript
+import Rimp from 'rimp'
+
+<Rrrrimp
+  buttonValue={`submit`}
+  buttonStyles={`btn`}
+  containerStyles={`newsletter__form`}
+  mailChimpUrl="//dirtylemondetox.us10.list-manage.com/subscribe/post?u=9b16ac3d614016ac03988b5bc&amp;id=6cd87d0164"
+ />
+```
+
+
+MIT License
---
+++ package/index.js
@@ -0,0 +1,132 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import reqwest from 'reqwest'
+
+const { string } = PropTypes
+
+export default class Rimp extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      buttonValue: props.buttonValue,
+      buttonStyles: props.buttonStyles,
+      containerStyles: props.containerStyles,
+      placeholder: props.placeholder,
+      inputStyles: props.inputStyles,
+      formWrapper: props.formWrapper,
+      valid: false,
+      isTyping: false,
+      showError: props.showError,
+      helpText: props.helpText,
+      mailChimpUrl: props.mailChimpUrl,
+      submitted: false
+    }
+
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.validateEmail = this.validateEmail.bind(this)
+    this.returnStyles = this.returnStyles.bind(this)
+  }
+
+  static get defaultProps () {
+    return {
+      buttonValue: 'submit',
+      buttonStyles: 'button',
+      placeholder: 'enter your email address',
+      formWrapper: 'flex flex-justify-between flex-align-center',
+      containerStyles: 'newsletter__form',
+      helpText: 'Please provide a valid email address',
+      showError: true,
+      emailAddress: '',
+      mailChimpUrl: null
+    }
+  }
+
+  handleSubmit (e) {
+    e.preventDefault()
+    let self = this
+    let isValid = this.state.valid
+    if (isValid) {
+      reqwest({
+        method: 'get',
+        type: 'jsonp',
+        contentType: 'application/json',
+        url: this.state.mailChimpUrl,
+        data: {EMAIL: this.state.emailAddress, STATUS: 'subscribed'},
+        jsonpCallback: 'c'
+      })
+      .then(function (resp) {
+        self.setState({
+          submitted: true
+        })
+      })
+      .fail(function (err, msg) {
+        console.log(err)
+        self.setState({
+          submitted: true
+        })
+      })
+      .always(function (resp) {
+        self.setState({
+          submitted: true
+        })
+      })
+      self.setState({
+        submitted: true
+      })
+    }
+  }
+
+  validateEmail (e) {
+    this.setState({
+      isTyping: true
+    })
+    function validateEmail (email) {
+      var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      return re.test(email)
+    }
+    if (validateEmail(e.currentTarget.value)) {
+      this.setState({
+        valid: true,
+        emailAddress: e.currentTarget.value
+      })
+    } else {
+      this.setState({
+        valid: false
+      })
+    }
+  }
+
+  returnStyles () {
+    let styles = `${this.state.containerStyles}`
+    if (this.state.isTyping) {
+      styles += ` ${this.state.valid ? 'valid' : 'not-valid'}`
+    }
+    return styles
+  }
+
+  render () {
+    return (
+      <div className={this.returnStyles()}>
+        {!this.state.submitted
+          ? <div>
+            <form onSubmit={this.handleSubmit} className={this.state.formWrapper}>
+              <input type='email' onChange={this.validateEmail} ref='email' className={this.state.inputStyles} placeholder={this.state.placeholder} />
+              <button className={this.state.buttonStyles}>{this.state.buttonValue}</button>
+            </form>
+            {this.state.showError
+              ? <div>
+                {!this.state.valid && this.state.isTyping
+                  ? <div className='error'>{this.state.helpText}</div>
+                : null}
+              </div>
+            : null}
+          </div>
+        : <p>You’ve been added to our list. X</p> }
+      </div>
+    )
+  }
+}
+
+Rimp.propTypes = {
+  mailChimpUrl: string.isRequired
+}
